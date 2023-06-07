895094f243c91a33fac6f688de27acfe4d522880
Juan de Dios Martínez Vallejo <vallemardeveloper@gmail.com>
2023-06-08 01:21:26 +0200

    feat: add useRootLayout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,6 +6,7 @@ import { StackLayout } from "@nativescript/core";
 import ElementSizeView from "~/components/ElementSizeView.vue";
 import ColorModeView from "~/components/ColorModeView.vue";
 import EventListenerComponent from "~/components/EventListenerComponent.vue";
+import RootLayoutView from './rootLayout/RootLayoutView.vue';
 
 function onUseElementSize() {
   $navigateTo(ElementSizeView)
@@ -18,6 +19,10 @@ function onUseEventListener() {
   $navigateTo(EventListenerComponent)
 }
 
+function onUseRootLayout() {
+  $navigateTo(RootLayoutView)
+}
+
 </script>
 
 <template>
@@ -26,9 +31,10 @@ function onUseEventListener() {
       <StackLayout ref="test" class="px-4">
         <Label text="NativeScript-VueUse APP" class="text-2xl font-bold mt-3" />
 
-        <Button @tap="onUseElementSize" text="UseElementSize" class="mt-12 m-1"></Button>
-        <Button @tap="onUseColorMode" text="UseColorMode" class="mt-8 m-1"></Button>
-        <Button @tap="onUseEventListener" text="UseEventListener" class="mt-8 m-1"></Button>
+        <Button @tap="onUseElementSize" text="Use Element Size" class="mt-12 m-1"></Button>
+        <Button @tap="onUseColorMode" text="Use Color Mode" class="mt-8 m-1"></Button>
+        <Button @tap="onUseEventListener" text="Use Event Listener" class="mt-8 m-1"></Button>
+        <Button @tap="onUseRootLayout" text="Use Root Layout" class="mt-8 m-1"></Button>
       </StackLayout>
     </Page>
   </Frame>
---
+++ src/components/rootLayout/RootLayoutView.vue
@@ -0,0 +1,116 @@
+<script lang="ts" setup>
+import { ref, onMounted } from 'nativescript-vue'
+import { useRootLayout } from '@vallemar/nativescript-vueuse'
+import BottomSheet from "./BottomSheet.vue";
+import Modal from './Modal.vue';
+import SideBar from './SideBar.vue';
+import SnackBar from './SnackBar.vue';
+
+
+const { show: showBottomSheet } = useRootLayout(BottomSheet, {
+  rootLayoutOption: {
+    shadeCover: {
+      color: '#282C34',
+      opacity: 0.7,
+      tapToClose: true,
+    },
+    animation: {
+      enterFrom: {
+        translateY: 500,
+        duration: 300,
+      },
+      exitTo: {
+        translateY: 500,
+        duration: 300,
+      },
+    },
+  },
+  onClose: () => {
+    console.log("onClose BottomSheet")
+  }
+});
+
+const { show: showModal } = useRootLayout(Modal, {
+  rootLayoutOption: {
+    shadeCover: {
+      color: '#282C34',
+      opacity: 0.7,
+      tapToClose: true,
+    },
+    animation: {
+      enterFrom: {
+        translateY: -200,
+        duration: 300,
+      },
+      exitTo: {
+        translateY: -200,
+        opacity: 0,
+        duration: 300,
+      },
+    },
+  },
+  onClose: () => {
+    console.log("onClose Modal")
+  }
+});
+
+const { show: showSideBar } = useRootLayout(SideBar, {
+  rootLayoutOption: {
+    shadeCover: {
+      color: '#282C34',
+      opacity: 0.7,
+      tapToClose: true,
+    },
+    animation: {
+      enterFrom: {
+        translateX: -300,
+        duration: 300,
+      },
+      exitTo: {
+        translateX: -300,
+        duration: 300,
+      },
+    },
+  },
+  onClose: () => {
+    console.log("onClose SideBar")
+  }
+});
+const { show: showSnackbar } = useRootLayout(SnackBar, {
+  rootLayoutOption: {
+    shadeCover: {
+      color: '#282C34',
+      opacity: 0.7,
+      tapToClose: true,
+    },
+    animation: {
+      enterFrom: {
+        translateY: -300,
+        duration: 300,
+      },
+      exitTo: {
+        translateY: -300,
+        duration: 300,
+      },
+    },
+  },
+  onClose: () => {
+    console.log("onClose SnackBar")
+  }
+});
+</script>
+
+<template>
+  <Page actionBarHidden="true">
+    <RootLayout>
+      <StackLayout class="p-3">
+        <Label class="text-center text-2xl">RootLayout example</Label>
+        <Button @tap="showBottomSheet" class="mx-1 mt-8" text="Open BottomSheet"></Button>
+        <Button @tap="showModal" class="mx-1 mt-8" text="Open Modal"></Button>
+        <Button @tap="showSideBar" class="mx-1 mt-8" text="Open Sidebar"></Button>
+        <Button @tap="showSnackbar" class="mx-1 mt-8" text="Open Snackbar"></Button>
+
+      </StackLayout>
+    </RootLayout>
+  </Page>
+</template>
