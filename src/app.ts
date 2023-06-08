ae2f6b9cb2471021347d8aabc780d61d44472fb4
Juan de Dios Martínez Vallejo <vallemardeveloper@gmail.com>
2023-06-08 02:38:35 +0200

    chore: upate

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'nativescript-vue';
+import { useColorMode } from "@vallemar/nativescript-vueuse";
 import Home from './components/Home.vue';
-import {useColorMode} from "@vallemar/nativescript-vueuse";
-import {THEMES} from "~/data";
+import { THEMES } from "~/data";
 
 useColorMode({
     modes: THEMES
