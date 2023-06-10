525f90f22945be43813456a76b56d895490a6546
Juan de Dios Martínez Vallejo <vallemardeveloper@gmail.com>
2023-06-10 13:29:41 +0200

    chore: upate

--- src/components/ColorModeView.vue
+++ src/components/ColorModeView.vue
@@ -21,9 +21,9 @@ function changeTheme(newTheme: any) {
 <template>
   <CustomPage>
     <StackLayout class="py-4 pl-3">
-      <Label :text="`Current Theme: ${schema}`" />
+      <Label :text="`Current Theme: ${schema}`" class="text-2xl" />
 
-      <ListView :items="THEMES" height="400">
+      <ListView :items="THEMES" height="300">
         <template #default="{ item, index }: ListItem<string>">
           <FlexboxLayout class="pl-2 py-4" @tap="changeTheme(item)">
             <Label class="h-[16] w-[16] bg-green-400 rounded-full"
@@ -32,7 +32,12 @@ function changeTheme(newTheme: any) {
           </FlexboxLayout>
         </template>
       </ListView>
-      <FlexboxLayout class=" justify-around">
+
+      <FlexboxLayout>
+        <Label text="Reactive Palette" width="auto" class="mt-8 text-lg px-2 rounded-lg" :backgroundColor=" palette?.colors.bgVariant " />
+      </FlexboxLayout>
+
+      <FlexboxLayout class="justify-around mt-4">
         <StackLayout :backgroundColor=" palette?.colors.bgVariant " class="px-3 py-2 rounded-full  text-center">
           <Label text="bg"></Label>
           <Label :text=" palette?.colors.bg "></Label>
