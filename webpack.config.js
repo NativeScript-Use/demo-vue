89d706276f039e7e1016410f949c5a060f3142d7
Juan de Dios Martínez Vallejo <vallemardeveloper@gmail.com>
2023-06-17 01:17:01 +0200

    chore: update app to new workspace

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,33 @@
-const webpack = require("@nativescript/webpack");
-const {resolve} = require("path");
+const webpack = require('@nativescript/webpack');
+const { resolve } = require('path');
 
 module.exports = (env) => {
-    webpack.init(env);
+  webpack.init(env);
 
-    webpack.chainWebpack((config) => {
-        const isPreview = !!env.preview;
-        if(!isPreview){
-            config.resolve.alias.set(
-                '@vallemar/nativescript-vueuse',
-                resolve(__dirname, '../publish')
-            );
-        }
-    });
+  webpack.chainWebpack((config) => {
+    const isPreview = !!env.preview;
+    if (!isPreview) {
+      config.resolve.alias.set(
+        '@vallemar/nativescript-vueuse',
+        resolve(__dirname, '../../packages/nativescript-vueuse')
+      );
 
-    return webpack.resolveConfig();
+      config.resolve.alias.set(
+        '@vallemar/nativescript-orientation',
+        resolve(__dirname, '../../packages/nativescript-orientation')
+      );
+
+      config.resolve.alias.set(
+        '@vallemar/nativescript-keyboard',
+        resolve(__dirname, '../../packages/nativescript-keyboard')
+      );
+
+      config.resolve.alias.set(
+        '@vallemar/nativescript-clipboard',
+        resolve(__dirname, '../../packages/nativescript-clipboard')
+      );
+    }
+  });
+
+  return webpack.resolveConfig();
 };
