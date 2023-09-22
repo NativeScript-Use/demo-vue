62fd5e595609e9c5ad90f53f2364e160c887d9b7
Juan de Dios Martínez Vallejo <vallemardeveloper@gmail.com>
2023-09-22 15:31:09 +0200

    chore: update style on Home screen

--- src/components/FeatureItem.vue
+++ src/components/FeatureItem.vue
@@ -9,10 +9,14 @@ const { feature } = defineProps({
     }
 })
 
+function getRandomColor() {
+  return `hsla(${~~(360 * Math.random())}, 70%,  72%, 0.8)`;
+}
+
 </script>
 <template>
-    <FlexboxLayout class="bg-variant p-2  py-4 rounded-lg flex-col">
-        <Icon width="32" class="" :icon="feature.icon"></Icon>
-        <Label :text="feature.title" class="text-xl font-bold mt-4" textWrap="true" style="line-height: 1;" />
+    <FlexboxLayout class="p-2  py-4 rounded-lg flex-col" :style="{ background: getRandomColor() }">
+        <Icon width="32" class="text-black" :icon="feature.icon"></Icon>
+        <Label :text="feature.title" class="text-xl text-black font-bold mt-4" textWrap="true" style="line-height: 1;" />
     </FlexboxLayout>
 </template>
